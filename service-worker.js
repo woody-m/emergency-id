00244c49430145ce0a42a89028e30b2ccf9d37e8
woody-m <bwoodymila@gmail.com>
2026-01-30 21:08:54 -0400

    feat: enable offline support with PWA configuration / habilitar funcionamento offline com configuração PWA

---
+++ service-worker.js
@@ -0,0 +1,23 @@
+const CACHE = "emergency-v1";
+
+const ASSETS = [
+    "/",
+    "/index.html",
+    "/css/styles.css",
+    "/js/app.js",
+    "/js/tts.js",
+    "/assets/img/autism-bg.png",
+    "/assets/img/child.jpg"
+];
+
+self.addEventListener("install", e => {
+    e.waitUntil(
+        caches.open(CACHE).then(cache => cache.addAll(ASSETS))
+    );
+});
+
+self.addEventListener("fetch", e => {
+    e.respondWith(
+        caches.match(e.request).then(res => res || fetch(e.request))
+    );
+});
